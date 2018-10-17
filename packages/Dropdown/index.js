a3175c46ab51cc8daa96d25531d37f0c2fb0eee1
Adrian Unger <dev@staydecent.ca>
2018-10-17 11:42:08 -0700

    Add Dropdown.

---
+++ packages/Dropdown/index.js
@@ -0,0 +1,90 @@
+import W from 'wasmuth'
+import React from 'react' // Can be aliased to `preact` in host project
+import {connect} from 'unistore/react' // Can alias to `unistore/preact` in host project
+
+import Level from '@app-elements/level'
+
+import './style.less'
+
+let storeRef // Will get populated by `getStoreReference`
+
+const getStoreReference = actions => store => {
+  storeRef = store
+  return actions
+}
+
+const actions = getStoreReference({
+  setDropdown: (state, dropdown) => ({dropdown}),
+  handleClick: uid => (state, event) => {
+    event.preventDefault()
+    const isOpen = state.dropdown === uid
+    return {dropdown: isOpen ? null : uid}
+  }
+})
+
+const mapper = ({dropdown}, {uid}) => {
+  if (!uid) {
+    console.warn('<Dropdown> must include a uid prop.')
+  }
+  return {isOpen: dropdown === uid}
+}
+
+const Dropdown = connect(mapper, actions)(({
+  // Store
+  isOpen,
+  setDropdown,
+  handleClick,
+
+  // Props
+  Trigger,
+  uid,
+  buttonText = 'Select',
+  noWrapper = false,
+  children
+}) => {
+  const cls = isOpen
+    ? 'dropdown-menu open'
+    : isOpen === false
+      ? 'dropdown-menu close'
+      : 'dropdown-menu' // isOpen === null
+  return (
+    <div>
+      {Trigger === undefined
+        ? <button className='btn btn-dropdown black-ghost-btn' onClick={handleClick(uid)}>
+          <Level noPadding>{buttonText}</Level>
+        </button>
+        : <Trigger className='btn-dropdown' onClick={handleClick(uid)} />}
+      {noWrapper
+        ? isOpen && children
+        : <div className={cls}>
+          <div class='dropdown-arrow' />
+          {children}
+        </div>}
+    </div>
+  )
+})
+
+export default Dropdown
+
+// DOM event to close all Dropdown's on off-click
+const isDropdown = (el) =>
+  (el.classList && el.classList.contains('dropdown-menu')) ||
+  (el.classList && el.classList.contains('btn-dropdown'))
+
+try {
+  document.body.addEventListener('click', (ev) => {
+    if (!storeRef) return
+    const activeDropdown = W.path('dropdown', storeRef.getState())
+    console.log({activeDropdown})
+    if (!activeDropdown) {
+      return
+    }
+    let el = ev.target
+    if (isDropdown(el)) return
+    while (el.parentNode) {
+      el = el.parentNode
+      if (isDropdown(el)) return
+    }
+    storeRef.setState({dropdown: null})
+  })
+} catch (_) {}
