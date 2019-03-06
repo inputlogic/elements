1515958efc866fafe029df3d5351d10f5b7723de
Adrian Unger <dev@staydecent.ca>
2019-03-06 12:42:01 -0800

    Update list-resource to make request via connect.

--- components/list-resource/index.js
+++ components/list-resource/index.js
@@ -3,56 +3,54 @@ import W from 'wasmuth'
 import connect from '@app-elements/connect'
 import Pagination from '@app-elements/pagination'
 import qs from '@app-elements/router/qs'
-import WithRequest from '@app-elements/with-request'
+
+const getActivePage = () => {
+  const search = typeof window !== 'undefined' ? window.location.search : ''
+  const args = qs.parse(search)
+  return args.page ? parseInt(args.page, 10) : 1
+}
 
 // We subscribe to `currentPath` to rerender on route change
 const ListResource = connect({
   name: 'ListResource',
-  withState: ({ currentPath }) => ({ currentPath })
+  withState: ({ currentPath }) => ({ currentPath }),
+  withRequest: {
+    endpoint: ({ limit, endpoint }) => {
+      const activePage = getActivePage()
+      return limit != null
+        ? `${endpoint}?limit=${limit}${activePage > 1 ? `&offset=${limit * activePage}` : ''}`
+        : endpoint
+    }
+  }
 })(({
   endpoint,
   limit,
   list = true,
   pagination = false,
+  result,
+  isLoading,
   render: View
-}) => {
-  // @TODO: Needs to access search params on SSR
-  const search = typeof window !== 'undefined' ? window.location.search : ''
-  const args = qs.parse(search)
-  const activePage = args.page ? parseInt(args.page, 10) : 1
-
-  const request = {
-    endpoint: limit != null
-      ? `${endpoint}?limit=${limit}${activePage > 1 ? `&offset=${limit * activePage}` : ''}`
-      : endpoint
-  }
-
-  return (
-    <WithRequest request={request}>
-      {({ result, isLoading }) =>
-        isLoading
-          ? <p>Loading...</p>
-          : <div key={request.endpoint}>
-            {list
-              ? W.map(
-                item => <View {...item} />,
-                W.pathOr(result, 'results', result)
-              )
-              : <View {...result} />
-            }
-            {pagination && limit != null
-              ? <Pagination
-                activePage={activePage}
-                request={result}
-                pageSize={limit}
-              />
-              : null
-            }
-          </div>
+}) =>
+  isLoading
+    ? <p>Loading...</p>
+    : <div key={endpoint}>
+      {list
+        ? W.map(
+          item => <View {...item} />,
+          W.pathOr(result, 'results', result)
+        )
+        : <View {...result} />
       }
-    </WithRequest>
-  )
-})
+      {pagination && limit != null
+        ? <Pagination
+          activePage={getActivePage()}
+          request={result}
+          pageSize={limit}
+        />
+        : null
+      }
+    </div>
+)
 
 export default ListResource
 
