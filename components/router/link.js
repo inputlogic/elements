1b741328411d5e5398480c913b5c6e6b702127c0
Adrian Unger <dev@staydecent.ca>
2019-04-16 10:57:17 -0700

    Add Link to Router.

---
+++ components/router/link.js
@@ -0,0 +1,43 @@
+import React from 'react' // Can be aliased to Preact in your project
+
+import qs from './qs'
+
+let allRoutes
+
+const getAllRoutes = routes =>
+  Object
+    .keys(routes || {})
+    .reduce((acc, r) =>
+      routes[r].hasOwnProperty('routes')
+        ? { ...acc, ...getAllRoutes(routes[r].routes) }
+        : { ...acc, [r]: routes[r] },
+    {})
+
+export class Link extends React.Component {
+  render () {
+    const { routes } = this.context
+    const { name, args = {}, queries = {}, children, ...props } = this.props
+
+    if (allRoutes == null) {
+      allRoutes = getAllRoutes(routes)
+    }
+
+    const rule = allRoutes[name]
+    if (!rule) {
+      console.warn('No route found for name: ' + name)
+      return
+    }
+
+    const replaced = Object
+      .keys(args)
+      .reduce((acc, k) => acc.replace(`:${k}`, args[k]), rule.path)
+    const hasQueries = Object.keys(queries).length > 0
+    const href = `${replaced}${!hasQueries ? '' : '?' + qs.stringify(queries)}`
+
+    return (
+      <a href={href} {...props}>{children}</a>
+    )
+  }
+}
+
+export default Link
