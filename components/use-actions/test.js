92fb5bac44f38ae7089e7b4210335daeb9bd0674
Adrian Unger <dev@staydecent.ca>
2019-10-15 13:16:21 -0700

    Rough in useActions hook.

---
+++ components/use-actions/test.js
@@ -0,0 +1,52 @@
+/* global afterEach test expect */
+
+import { render } from 'preact'
+import createStore from 'atom'
+import { useMappedState } from './index.js'
+
+const store = createStore([], { count: 0 })
+
+const Stateful = (props) => {
+  const { count } = useMappedState(store, ({ count }) => ({ count }))
+  return (
+    <p>Count: {count}</p>
+  )
+}
+
+afterEach(() => {
+  store.setState({ count: 0 })
+  document.body.innerHTML = ''
+})
+
+test('useMappedState exports', () => {
+  expect(typeof useMappedState).toBe('function')
+})
+
+test('useMappedState should render initial state', () => {
+  render(<Stateful />, document.body)
+  expect(document.body.innerHTML).toBe('<p>Count: 0</p>')
+})
+
+test('useMappedState should update when mapped state changes', (done) => {
+  const Child = ({ count }) => <p>Count: {count}</p>
+  const Parent = () => {
+    const { count } = useMappedState(store, ({ count }) => ({ count }))
+    return <div><Child count={count} /></div>
+  }
+
+  const listener = () => {
+    render(<Parent />, document.body)
+    setTimeout(() => {
+      expect(store.getState()).toEqual({ count: 1 })
+      expect(document.body.innerHTML).toBe('<div><p>Count: 1</p></div>')
+      store.unsubscribe(listener)
+      done()
+    }, 1000)
+  }
+
+  render(<Parent />, document.body)
+  expect(document.body.innerHTML).toBe('<div><p>Count: 0</p></div>')
+
+  store.subscribe(listener)
+  store.setState({ count: 1 })
+})
