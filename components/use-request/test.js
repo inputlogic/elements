10a7c489329ba504b2f77f87af32e1280f82d71a
Adrian Unger <dev@staydecent.ca>
2019-10-16 12:27:47 -0700

    Test multiple useRequests per Component.

--- components/use-request/test.js
+++ components/use-request/test.js
@@ -28,14 +28,46 @@ test('useRequest should render PassedComponent', (done) => {
   }
 
   // Wait for store to update
-  store.subscribe(() => {
+  const listener = () => {
     // Wait for React to re-render with updated state
     setTimeout(() => {
       render(<Context.Provider value={store}><Requested /></Context.Provider>, document.body)
       expect(document.body.innerHTML).toBe('<h1>User: Mark</h1>')
       done()
     }, 2000)
-  })
+    store.unsubscribe(listener)
+  }
+  store.subscribe(listener)
+
+  render(<Context.Provider value={store}><Requested /></Context.Provider>, document.body)
+  expect(document.body.innerHTML).toBe('<p>Loading...</p>')
+})
+
+test('useRequest can be used more than once per Component', (done) => {
+  const Requested = (props) => {
+    const store = useContext(Context)
+    const { result: user1 } = useRequest(store, '/users/4')
+    const { result: user2 } = useRequest(store, '/users/5')
+    return (
+      <Fragment>
+        {user1 == null || user2 == null
+          ? <p>Loading...</p>
+          : <h1>{user1.name}/{user2.name}</h1>}
+      </Fragment>
+    )
+  }
+
+  // Wait for store to update
+  const listener = () => {
+    // Wait for React to re-render with updated state
+    setTimeout(() => {
+      render(<Context.Provider value={store}><Requested /></Context.Provider>, document.body)
+      expect(document.body.innerHTML).toBe('<h1>Mark/Paul</h1>')
+      done()
+    }, 2000)
+    store.unsubscribe(listener)
+  }
+  store.subscribe(listener)
 
   render(<Context.Provider value={store}><Requested /></Context.Provider>, document.body)
   expect(document.body.innerHTML).toBe('<p>Loading...</p>')
