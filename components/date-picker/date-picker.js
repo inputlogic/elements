eee18a7e2bad2804271003677d6712fc69f1e819
Adrian Unger <dev@staydecent.ca>
2020-03-20 16:33:11 -0700

    WIP add DateRangePicker.

--- components/date-picker/date-picker.js
+++ components/date-picker/date-picker.js
@@ -6,45 +6,35 @@ import './date-picker.less'
 
 const dayStrings = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
 
-export function DatePicker ({ weekStartDay = 0, selectedDate, onChange }) {
-  if (selectedDate != null && typeof selectedDate !== 'number') {
-    console.error('DatePicker only accepts a timestamp for selectedDate!')
-    return null
-  }
-  const [date, setDate] = useState(new Date())
-
+export function BaseDatePicker ({
+  weekStartDay,
+  date,
+  clsNames,
+  onNextMonth,
+  onPrevMonth,
+  onSelect
+}) {
   const calendar = buildCalendar(weekStartDay, date)
   const dayHeaders = weekStartDay === 0
     ? dayStrings.slice(0)
     : dayStrings.slice(weekStartDay).concat(dayStrings.slice(0, weekStartDay))
 
-  const isCurrentMonth = d => d.getMonth() === date.getMonth()
-  const isToday = d =>
-    d.getMonth() === (new Date()).getMonth() && d.getDate() === (new Date()).getDate()
-  const isSelected = d => selectedDate != null && d.getTime() === selectedDate
-
-  const clsNames = (day) => [
-    'ae-date-picker-date',
-    isToday(day) && 'ae-date-picker-today',
-    !isCurrentMonth(day) && 'ae-date-picker-other',
-    isSelected(day) && 'ae-date-picker-selected'
-  ].filter(Boolean).join(' ')
-
   const handleNextMonth = (event) => {
     event.preventDefault()
     event.stopPropagation()
-    setDate(addMonths(1, date))
+    onNextMonth()
   }
 
   const handlePrevMonth = (event) => {
     event.preventDefault()
     event.stopPropagation()
-    setDate(subMonths(1, date))
+    onPrevMonth()
   }
 
   const handleSelect = (day) => (event) => {
     event.preventDefault()
-    onChange(day)
+    event.stopPropagation()
+    onSelect(day)
   }
 
   return (
@@ -75,5 +65,98 @@ export function DatePicker ({ weekStartDay = 0, selectedDate, onChange }) {
         </table>
       </div>
     </div>
+  )
+}
+
+export function DatePicker ({ weekStartDay = 0, selectedDate, onChange }) {
+  if (selectedDate != null && typeof selectedDate !== 'number') {
+    console.error('DatePicker only accepts a timestamp for selectedDate!')
+    return null
+  }
+
+  const [date, setDate] = useState(new Date())
+
+  const isCurrentMonth = d => d.getMonth() === date.getMonth()
+  const isToday = d =>
+    d.getMonth() === (new Date()).getMonth() && d.getDate() === (new Date()).getDate()
+  const isSelected = d => selectedDate != null && d.getTime() === selectedDate
+
+  const clsNames = (day) => [
+    'ae-date-picker-date',
+    isToday(day) && 'ae-date-picker-today',
+    !isCurrentMonth(day) && 'ae-date-picker-other',
+    isSelected(day) && 'ae-date-picker-selected'
+  ].filter(Boolean).join(' ')
+
+  const onNextMonth = () => {
+    setDate(addMonths(1, date))
+  }
+
+  const onPrevMonth = () => {
+    setDate(subMonths(1, date))
+  }
+
+  const onSelect = (day) => {
+    onChange(day)
+  }
+
+  return (
+    <BaseDatePicker
+      weekStartDay={weekStartDay}
+      date={date}
+      clsNames={clsNames}
+      onNextMonth={onNextMonth}
+      onPrevMonth={onPrevMonth}
+      onSelect={onSelect}
+    />
+  )
+}
+
+export function DateRangePicker ({ weekStartDay = 0, startDate, endDate, onChange }) {
+  if (startDate != null && typeof startDate !== 'number') {
+    console.error('DateRangePicker only accepts a timestamp for startDate!')
+    return null
+  }
+  if (endDate != null && typeof endDate !== 'number') {
+    console.error('DateRangePicker only accepts a timestamp for endDate!')
+    return null
+  }
+
+  const [date, setDate] = useState(new Date())
+
+  const isCurrentMonth = d => d.getMonth() === date.getMonth()
+  const isToday = d =>
+    d.getMonth() === (new Date()).getMonth() && d.getDate() === (new Date()).getDate()
+  // Should we select all days that fall in between start and end?
+  const isSelected = d => selectedDate != null && d.getTime() === selectedDate
+
+  const clsNames = (day) => [
+    'ae-date-picker-date',
+    isToday(day) && 'ae-date-picker-today',
+    !isCurrentMonth(day) && 'ae-date-picker-other',
+    isSelected(day) && 'ae-date-picker-selected'
+  ].filter(Boolean).join(' ')
+
+  const onNextMonth = () => {
+    setDate(addMonths(1, date))
+  }
+
+  const onPrevMonth = () => {
+    setDate(subMonths(1, date))
+  }
+
+  const onSelect = (day) => {
+    onChange(day)
+  }
+
+  return (
+    <BaseDatePicker
+      weekStartDay={weekStartDay}
+      date={date}
+      clsNames={clsNames}
+      onNextMonth={onNextMonth}
+      onPrevMonth={onPrevMonth}
+      onSelect={onSelect}
+    />
   )
 }
