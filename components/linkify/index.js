4e7a827f51da584fce5b8941ba34f1569eecad68
Adrian Unger <dev@staydecent.ca>
2020-05-20 12:11:15 -0700

    Fix linkify build.
    Need to set JSX and React as external.

--- components/linkify/index.js
+++ components/linkify/index.js
@@ -1,4 +1,6 @@
 /* eslint no-useless-escape: 0 */
+import React from 'react'
+
 const delimiter = /((?:https?:\/\/)?(?:(?:[a-z0-9]?(?:[a-z0-9\-]{1,61}[a-z0-9])?\.[^\.|\s])+[a-z\.]*[a-z]+|(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3})(?::\d{1,5})*[a-z0-9.,_\/~#&=;%+?\-\\(\\)]*)/ig
 
 const protocol = typeof window !== 'undefined'
